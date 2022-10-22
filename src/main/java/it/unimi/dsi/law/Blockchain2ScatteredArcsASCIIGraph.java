c0d5d279796eea138e577e5f71a5f5bf30175c04
Luigi Foscari <luigi.foscari@icloud.com>
2022-10-22 23:05:33 +0200

    Apparently giving the blockloader the specific blockfiles to load causes the blocks/s to drop by two orders of magnitude.

--- src/main/java/it/unimi/dsi/law/Blockchain2ScatteredArcsASCIIGraph.java
+++ src/main/java/it/unimi/dsi/law/Blockchain2ScatteredArcsASCIIGraph.java
@@ -11,20 +11,18 @@ import it.unimi.dsi.webgraph.ScatteredArcsASCIIGraph;
 import org.bitcoinj.core.*;
 import org.bitcoinj.params.MainNetParams;
 import org.bitcoinj.script.Script;
-import org.bitcoinj.script.ScriptException;
 import org.bitcoinj.utils.BlockFileLoader;
 import org.rocksdb.RocksDBException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
-import static org.bitcoinj.script.Script.ScriptType.P2PK;
 import static org.bitcoinj.script.Script.ScriptType.P2WPKH;
+import static org.bitcoinj.utils.BlockFileLoader.getReferenceClientBlockFileList;
 
 public class Blockchain2ScatteredArcsASCIIGraph implements Iterable<long[]> {
     public final NetworkParameters np;
@@ -71,7 +69,7 @@ public class Blockchain2ScatteredArcsASCIIGraph implements Iterable<long[]> {
         private final BlockFileLoader bfl;
         private final LongArrayFIFOQueue transactionArcs = new LongArrayFIFOQueue();
 
-        private final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance(Parameters.resources + "bitcoin-parser-db");
+        private final PersistenceLayer persistenceLayer = PersistenceLayer.getInstance(Parameters.resources + "bitcoin-db");
         private final AddressConversion addressConversion = persistenceLayer.getAddressConversion();
         private final IncompleteMappings incompleteMappings = persistenceLayer.getIncompleteMappings();
         private final TransactionOutpointFilter topFilter = persistenceLayer.getTransactionOutpointFilter();
@@ -82,10 +80,10 @@ public class Blockchain2ScatteredArcsASCIIGraph implements Iterable<long[]> {
             this.np = np;
             this.progress = progress;
 
-            List<File> blockFiles = getBlockFiles(blocksDirectory);
+            savedParsedBlocksFilenames(blocksDirectory, "parsed-blocks.txt");
 
             progress.start("First pass to populate mappings");
-            BlockFileLoader bflTemp = new BlockFileLoader(np, blockFiles);
+            BlockFileLoader bflTemp = new BlockFileLoader(np, new File(blocksDirectory));
 
             for (Block block : bflTemp) {
                 progress.update();
@@ -107,48 +105,51 @@ public class Blockchain2ScatteredArcsASCIIGraph implements Iterable<long[]> {
             progress.stop("Done populating the mappings of " + progress.count + " total blocks in " + progress.millis() / 1000  + " seconds");
             progress.start("Second pass to complete the mappings");
 
-            this.bfl = new BlockFileLoader(np, blockFiles);
+            this.bfl = new BlockFileLoader(np, new File(blocksDirectory));
         }
 
-        List<File> getBlockFiles(String directory) {
-            FilenameFilter blockFileFilter = (d, s) -> s.toLowerCase().startsWith("blk");
-            File[] blockFiles = (new File(directory)).listFiles(blockFileFilter);
+        void savedParsedBlocksFilenames(String blocksDirectory, String filename) {
+            try {
+                List<File> fileToParse = getReferenceClientBlockFileList(new File(blocksDirectory));
+                File out = new File(Parameters.resources + filename);
 
-            if (blockFiles == null)
-                throw new RuntimeException("No blocks found in " + directory + "!");
-
-            return List.of(blockFiles);
+                try (OutputStream os = new FileOutputStream(out)) {
+                    os.write(fileToParse.stream().map(File::getName).collect(Collectors.joining("\n")).getBytes());
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         List<Long> outputAddressesToLongs(Transaction t) throws RocksDBException {
             LongList outputs = new LongArrayList();
 
             for (TransactionOutput to : t.getOutputs()) {
-                Script script = null;
                 byte[] key;
 
                 try {
-                    script = to.getScriptPubKey();
+                    Script script = to.getScriptPubKey();
+                    Script.ScriptType type = script.getScriptType();
 
                     // If getToAddress fails I know that the script is not
                     // P2PKH, P2SH, P2PK, P2WH or P2TR, therefore is P2WPKH.
                     // We could handle this case with the catch, but it is
                     // significantly slower.
-                    if (script.getScriptType() == null || script.getScriptType().equals(P2WPKH)) {
+                    if (type == null || type.equals(P2WPKH)) {
                         // If the address is malformed in some way try identifying
                         // the receiver via the script, which contains the address
                         // and therefore cannot address someone else, but might not
                         // be unique to the public key.
                         // This cannot introduce wrong arcs, but in the worst case
                         // we get more junk in the database and miss some arcs.
-                        key = script.getProgram();
+                        key = script.getPubKeyHash();
                     } else {
                         Address receiver = script.getToAddress(this.np, true);
                         key = receiver.getHash();
                     }
                 } catch (RuntimeException e) {
                     // System.err.println(e.getClass().getCanonicalName() + ": " + e.getMessage());
-                    key = script != null ? script.getProgram() : ByteConversion.long2bytes(-1);
+                    key = ByteConversion.long2bytes(-1);
                 }
 
                 outputs.add(addressConversion.map(key));
