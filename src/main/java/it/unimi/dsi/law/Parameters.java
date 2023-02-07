1fb40ccd1b17c55195ec8cdc9481292334818741
Luigi Foscari <luigi.foscari@studenti.unimi.it>
2023-02-07 12:55:22 +0100

    server parameters

--- src/main/java/it/unimi/dsi/law/Parameters.java
+++ src/main/java/it/unimi/dsi/law/Parameters.java
@@ -7,7 +7,7 @@ public class Parameters {
 
 	// Paths
 
-	public final static Path resources = Path.of("src/main/resources");
+	public final static Path resources = Path.of("/mnt/extra/analysis/lfoscari/");
 	public final static Path artifacts = resources.resolve("artifacts");
 	public final static Path graph = resources.resolve("graph");
 
@@ -55,11 +55,11 @@ public class Parameters {
 
 	// Logging
 
-	public final static long logInterval = 10;
-	public final static TimeUnit logTimeUnit = TimeUnit.SECONDS;
+	public final static long logInterval = 5;
+	public final static TimeUnit logTimeUnit = TimeUnit.MINUTES;
 
-	/* public final static long WRITE_BUFFER_SIZE = 64 * SizeUnit.MB;
+	/* public final static long WRITE_BUFFER_SIZE = 36 * SizeUnit.GB;
 	public final static long MAX_TOTAL_WAL_SIZE = SizeUnit.GB;
-	public final static int MAX_BACKGROUND_JOBS = 5;
+	public final static int MAX_BACKGROUND_JOBS = 10;
 	public final static long MAX_BYTES_FOR_LEVEL_BASE = SizeUnit.GB; */
 }
