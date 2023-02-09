ef00f200c7ebf0f4f009b16ba03c4fbbd5ebc4df
Luigi Foscari <luigi.foscari@icloud.com>
2023-02-09 18:28:55 +0100

    fixed nodeutility

--- src/main/java/it/unimi/dsi/law/NodeUtility.java
+++ src/main/java/it/unimi/dsi/law/NodeUtility.java
@@ -9,32 +9,30 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.Random;
 
+import static it.unimi.dsi.law.Parameters.*;
+
 public class NodeUtility {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         GOVMinimalPerfectHashFunction<MutableString> addressMap =
-                (GOVMinimalPerfectHashFunction<MutableString>) BinIO.loadObject(Parameters.addressesMap.toFile());
+                (GOVMinimalPerfectHashFunction<MutableString>) BinIO.loadObject(addressesMap.toFile());
 
-        EFGraph bitcoinGraph = EFGraph.load(Parameters.basename.toString());
-        long[] ids = BinIO.loadLongs(Parameters.ids.toFile());
+        EFGraph bitcoinGraph = EFGraph.load(basename.toString());
+        long[] addressIds = BinIO.loadLongs(ids.toFile());
 
         Random r = new Random();
-        int node = r.nextInt(ids.length);
+        int node = r.nextInt(addressIds.length);
 
-        long id = ids[node];
+        long id = addressIds[node];
         int[] successors = bitcoinGraph.successorArray(node);
 
-        if (successors.length != bitcoinGraph.outdegree(node)) {
-            throw new RuntimeException();
-        }
-
-        System.out.println(id + " (" +  findAddress(addressMap, id) + ") -> [" + bitcoinGraph.outdegree(node) + "]");
+        System.out.println(findAddress(addressMap, id) + " (id: " + id + ", outdegree: " + bitcoinGraph.outdegree(node) + "):");
         for(int i = 0; i < bitcoinGraph.outdegree(node); i++) {
-            System.out.println("\t" + ids[successors[i]] + " (" +  findAddress(addressMap, ids[successors[i]]) + ")");
+            System.out.println("\t" + findAddress(addressMap, addressIds[successors[i]]) + " (id: " +  addressIds[successors[i]] + ")");
         }
     }
 
     static MutableString findAddress(GOVMinimalPerfectHashFunction<MutableString> addressMap, long id) {
-        Iterator<MutableString> it = Utils.readTSVs(Parameters.addressesFile);
+        Iterator<MutableString> it = Utils.readTSVs(addressesFile);
 
         while (it.hasNext()) {
             MutableString addr = it.next();
