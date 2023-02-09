a5755fa48e769a4ec72a5e25c82318a7f8654efc
Luigi Foscari <luigi.foscari@icloud.com>
2023-02-09 18:37:36 +0100

    nodeutility now uses the inverse address map to speed up the look up

--- src/main/java/it/unimi/dsi/law/NodeUtility.java
+++ src/main/java/it/unimi/dsi/law/NodeUtility.java
@@ -1,13 +1,16 @@
 package it.unimi.dsi.law;
 
 import it.unimi.dsi.fastutil.io.BinIO;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
 import it.unimi.dsi.lang.MutableString;
 import it.unimi.dsi.sux4j.mph.GOVMinimalPerfectHashFunction;
 import it.unimi.dsi.webgraph.EFGraph;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.Scanner;
 
 import static it.unimi.dsi.law.Parameters.*;
 
@@ -15,32 +18,25 @@ public class NodeUtility {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         GOVMinimalPerfectHashFunction<MutableString> addressMap =
                 (GOVMinimalPerfectHashFunction<MutableString>) BinIO.loadObject(addressesMap.toFile());
+        Long2ObjectOpenHashMap<String> addressInverseMap =
+                (Long2ObjectOpenHashMap<String>) BinIO.loadObject(addressesInverseMap.toFile());
 
         EFGraph bitcoinGraph = EFGraph.load(basename.toString());
         long[] addressIds = BinIO.loadLongs(ids.toFile());
 
-        Random r = new Random();
-        int node = r.nextInt(addressIds.length);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("address> ");
+        String address = sc.nextLine();
 
-        long id = addressIds[node];
+        long id = addressMap.getLong(address);
+        int node = ArrayUtils.indexOf(addressIds, id);
+
         int[] successors = bitcoinGraph.successorArray(node);
 
-        System.out.println(findAddress(addressMap, id) + " (id: " + id + ", outdegree: " + bitcoinGraph.outdegree(node) + "):");
+        System.out.println(address + " (id: " + id + ", outdegree: " + bitcoinGraph.outdegree(node) + "):");
         for(int i = 0; i < bitcoinGraph.outdegree(node); i++) {
-            System.out.println("\t" + findAddress(addressMap, addressIds[successors[i]]) + " (id: " +  addressIds[successors[i]] + ")");
+            long successorId = addressIds[successors[i]];
+            System.out.println("\t" + addressInverseMap.get(successorId) + " (id: " +  successorId + ")");
         }
-    }
-
-    static MutableString findAddress(GOVMinimalPerfectHashFunction<MutableString> addressMap, long id) {
-        Iterator<MutableString> it = Utils.readTSVs(addressesFile);
-
-        while (it.hasNext()) {
-            MutableString addr = it.next();
-            if (addressMap.getLong(addr) == id) {
-                return addr.copy();
-            }
-        }
-
-        return null;
     }
 }
