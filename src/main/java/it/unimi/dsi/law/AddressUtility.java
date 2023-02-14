2e25fab64c03b60b8a591e4b3c3b06dbf2a6cf05
Luigi Foscari <luigi.foscari@icloud.com>
2023-02-14 20:59:05 +0100

    added transaction utility, merged random node and address utility

--- src/main/java/it/unimi/dsi/law/AddressUtility.java
+++ src/main/java/it/unimi/dsi/law/AddressUtility.java
@@ -1,26 +1,30 @@
 package it.unimi.dsi.law;
 
 import com.google.common.collect.Iterators;
+import it.unimi.dsi.fastutil.BigArrays;
 import it.unimi.dsi.fastutil.io.BinIO;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.ObjectBigArrays;
+import it.unimi.dsi.lang.MutableString;
 import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.sux4j.mph.GOVMinimalPerfectHashFunction;
+import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
 import it.unimi.dsi.webgraph.EFGraph;
 import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Scanner;
 
 import static it.unimi.dsi.law.Parameters.*;
+import static it.unimi.dsi.law.Utils.buildInverseMap;
 
-public class NodeUtility {
-    public static final Logger logger = LoggerFactory.getLogger(NodeUtility.class);
+public class AddressUtility {
+    public static final Logger logger = LoggerFactory.getLogger(AddressUtility.class);
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         logger.info("Loading necessary data structures...");
@@ -30,35 +34,39 @@ public class NodeUtility {
             computeAddressInverseMap(addressMap);
         }
 
-        Long2ObjectOpenHashMap<String> addressInverseMap = (Long2ObjectOpenHashMap<String>) BinIO.loadObject(addressesInverseMap.toFile());
+        Object[][] addressInverseMap = (Object[][]) BinIO.loadObject(addressesInverseMap.toFile());
 
         EFGraph bitcoinGraph = EFGraph.load(basename.toString());
         long[] addressIds = BinIO.loadLongs(ids.toFile());
 
+        XoRoShiRo128PlusRandom r = new XoRoShiRo128PlusRandom();
+        Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.print("address> ");
-            String address = new Scanner(System.in).nextLine();
+            String address = sc.nextLine();
 
-            if (address.equals("stop")) {
-                return;
-            }
+            long id;
 
-            long id = addressMap.getLong(address);
+            if (address.equals("")) {
+                System.out.println("Picking a random address");
+                id = r.nextLong(addressMap.size64());
 
-            if (!addressInverseMap.get(id).equals(address)) {
-                logger.error("Address not found!");
-                continue;
+                if (!BigArrays.get(addressInverseMap, id).equals(address)) {
+                    logger.error("Address not found!");
+                    continue;
+                }
+            } else {
+                id = addressMap.getLong(address);
             }
 
             int node = ArrayUtils.indexOf(addressIds, id);
             int[] successors = bitcoinGraph.successorArray(node);
 
-            System.out.println(Arrays.toString(successors));
-
-            System.out.println(address + " (id: " + id + ", outdegree: " + bitcoinGraph.outdegree(node) + "):");
+            System.out.println(BigArrays.get(addressInverseMap, id) + " (id: " + id + ", outdegree: " + bitcoinGraph.outdegree(node) + "):");
             for (int i = 0; i < bitcoinGraph.outdegree(node); i++) {
                 long successorId = addressIds[successors[i]];
-                System.out.println("\t" + addressInverseMap.get(successorId) + " (id: " + successorId + ")");
+                System.out.println("\t" + BigArrays.get(addressInverseMap, successorId) + " (id: " + successorId + ")");
             }
             System.out.println();
         }
@@ -67,19 +75,9 @@ public class NodeUtility {
     private static void computeAddressInverseMap(GOVMinimalPerfectHashFunction<CharSequence> addressMap) throws IOException {
         logger.info("Computing inverse address map, this might take a while...");
         Iterator<CharSequence> addresses = Iterators.transform(Utils.readTSVs(addressesFile), line -> Utils.column(line, 0));
-        ProgressLogger progress = new ProgressLogger(LoggerFactory.getLogger(NodeUtility.class));
+        ProgressLogger progress = new ProgressLogger(LoggerFactory.getLogger(AddressUtility.class));
         progress.expectedUpdates = addressMap.size64();
         buildInverseMap(addressMap, addresses, addressesInverseMap, progress);
         logger.info("Done!");
-    }
-
-    private static void buildInverseMap(GOVMinimalPerfectHashFunction<CharSequence> map, Iterator<CharSequence> iterator, Path addressesInverseMap, ProgressLogger progress) throws IOException {
-        Long2ObjectOpenHashMap<String> inverse = new Long2ObjectOpenHashMap<>();
-        iterator.forEachRemaining(line -> {
-            inverse.put(map.getLong(line), line.toString());
-            progress.lightUpdate();
-        });
-        inverse.trim();
-        BinIO.storeObject(inverse, addressesInverseMap.toFile());
     }
 }
---
+++ src/main/java/it/unimi/dsi/law/TransactionUtility.java
@@ -0,0 +1,82 @@
+package it.unimi.dsi.law;
+
+import com.google.common.collect.Iterators;
+import it.unimi.dsi.fastutil.BigArrays;
+import it.unimi.dsi.fastutil.io.BinIO;
+import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.longs.LongOpenHashSet;
+import it.unimi.dsi.logging.ProgressLogger;
+import it.unimi.dsi.sux4j.mph.GOVMinimalPerfectHashFunction;
+import it.unimi.dsi.util.XoRoShiRo128PlusRandom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import static it.unimi.dsi.law.Parameters.*;
+import static it.unimi.dsi.law.Utils.buildInverseMap;
+
+public class TransactionUtility {
+    public static final Logger logger = LoggerFactory.getLogger(TransactionUtility.class);
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        GOVMinimalPerfectHashFunction<CharSequence> transactionsMap = (GOVMinimalPerfectHashFunction<CharSequence>) BinIO.loadObject(transactionsMapFile.toFile());
+
+        if (!transactionsInverseMapFile.toFile().exists()) {
+            computeTransactionInverseMap(transactionsMap);
+        }
+
+        Object[][] transactionsInverseMap = (Object[][]) BinIO.loadObject(transactionsInverseMapFile.toFile());
+
+        Long2ObjectOpenHashMap<LongOpenHashSet> transactionInputs = (Long2ObjectOpenHashMap<LongOpenHashSet>) BinIO.loadObject(transactionInputsFile.toFile());
+        Long2ObjectOpenHashMap<LongOpenHashSet> transactionOutputs = (Long2ObjectOpenHashMap<LongOpenHashSet>) BinIO.loadObject(transactionOutputsFile.toFile());
+
+        Scanner sc = new Scanner(System.in);
+        XoRoShiRo128PlusRandom r = new XoRoShiRo128PlusRandom();
+
+        while (true) {
+            System.out.print("transaction> ");
+            String transaction = sc.nextLine();
+
+            long transactionId;
+
+            if (transaction.equals("")) {
+                System.out.println("Picking a random transaction");
+                transactionId = r.nextLong(transactionsMap.size64());
+            } else {
+                transactionId = transactionsMap.getLong(transaction);
+            }
+
+            System.out.println(BigArrays.get(transactionsInverseMap, transactionId) + " (id: " + transactionId + ")");
+
+            LongOpenHashSet inputs = transactionInputs.get(transactionId);
+            System.out.println("Inputs (" + inputs.size() + "):");
+            for (long addressId: inputs) {
+                System.out.println("\t" + addressId);
+            }
+
+            LongOpenHashSet outputs = transactionOutputs.get(transactionId);
+            System.out.println("Outputs (" + outputs.size() + "):");
+            for (long addressId: outputs) {
+                System.out.println("\t" + addressId);
+            }
+
+            System.out.print("\nAnother? [y/n] ");
+            if (!sc.nextLine().equals("y")) {
+                return;
+            }
+        }
+    }
+
+    private static void computeTransactionInverseMap(GOVMinimalPerfectHashFunction<CharSequence> transactionMap) throws IOException {
+        logger.info("Computing inverse transaction map, this might take a while...");
+        Utils.LineFilter filter = (line) -> Utils.column(line, 7).equals("0");
+        Iterator<CharSequence> transactions = Iterators.transform(Utils.readTSVs(transactionsDirectory.toFile().listFiles(), filter), line -> Utils.column(line, 1));
+        ProgressLogger progress = new ProgressLogger(LoggerFactory.getLogger(AddressUtility.class));
+        progress.expectedUpdates = transactionMap.size64();
+        buildInverseMap(transactionMap, transactions, transactionsInverseMapFile, progress);
+        logger.info("Done!");
+    }
+}
