244d72ee9289c3ae5708d821d80835ad1c006669
Luigi Foscari <luigi.foscari@icloud.com>
2023-02-14 12:36:21 +0100

    loop nodeutility, progress logger improved

--- src/main/java/it/unimi/dsi/law/NodeUtility.java
+++ src/main/java/it/unimi/dsi/law/NodeUtility.java
@@ -1,10 +1,9 @@
 package it.unimi.dsi.law;
 
-import com.google.common.collect.Iterables;
 import com.google.common.collect.Iterators;
 import it.unimi.dsi.fastutil.io.BinIO;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
-import it.unimi.dsi.lang.MutableString;
+import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.sux4j.mph.GOVMinimalPerfectHashFunction;
 import it.unimi.dsi.webgraph.EFGraph;
 import org.apache.commons.lang3.ArrayUtils;
@@ -15,9 +14,7 @@ import java.io.IOException;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Iterator;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
-import java.util.function.Function;
 
 import static it.unimi.dsi.law.Parameters.*;
 
@@ -30,10 +27,7 @@ public class NodeUtility {
         GOVMinimalPerfectHashFunction<CharSequence> addressMap = (GOVMinimalPerfectHashFunction<CharSequence>) BinIO.loadObject(addressesMap.toFile());
 
         if (!addressesInverseMap.toFile().exists()) {
-            logger.info("Computing inverse address map, this might take a while...");
-            Iterator<CharSequence> addresses = Iterators.transform(Utils.readTSVs(addressesFile), line -> Utils.column(line, 0));
-            buildInverseMap(addressMap, addresses, addressesInverseMap);
-            logger.info("Done!");
+            computeAddressInverseMap(addressMap);
         }
 
         Long2ObjectOpenHashMap<String> addressInverseMap = (Long2ObjectOpenHashMap<String>) BinIO.loadObject(addressesInverseMap.toFile());
@@ -41,27 +35,46 @@ public class NodeUtility {
         EFGraph bitcoinGraph = EFGraph.load(basename.toString());
         long[] addressIds = BinIO.loadLongs(ids.toFile());
 
-        System.out.print("address> ");
-        String address = new Scanner(System.in).nextLine();
-        long id = addressMap.getLong(address);
+        while (true) {
+            System.out.print("address> ");
+            String address = new Scanner(System.in).nextLine();
 
-        if (!addressInverseMap.get(id).equals(address)) {
-            throw new NoSuchFileException("Address not found!");
-        }
+            if (address.equals("stop")) {
+                return;
+            }
 
-        int node = ArrayUtils.indexOf(addressIds, id);
-        int[] successors = bitcoinGraph.successorArray(node);
+            long id = addressMap.getLong(address);
 
-        System.out.println(address + " (id: " + id + ", outdegree: " + bitcoinGraph.outdegree(node) + "):");
-        for(int i = 0; i < bitcoinGraph.outdegree(node); i++) {
-            long successorId = addressIds[successors[i]];
-            System.out.println("\t" + addressInverseMap.get(successorId) + " (id: " +  successorId + ")");
+            if (!addressInverseMap.get(id).equals(address)) {
+                throw new NoSuchFileException("Address not found!");
+            }
+
+            int node = ArrayUtils.indexOf(addressIds, id);
+            int[] successors = bitcoinGraph.successorArray(node);
+
+            System.out.println(address + " (id: " + id + ", outdegree: " + bitcoinGraph.outdegree(node) + "):");
+            for (int i = 0; i < bitcoinGraph.outdegree(node); i++) {
+                long successorId = addressIds[successors[i]];
+                System.out.println("\t" + addressInverseMap.get(successorId) + " (id: " + successorId + ")");
+            }
         }
     }
 
-    private static void buildInverseMap(GOVMinimalPerfectHashFunction<CharSequence> map, Iterator<CharSequence> iterator, Path addressesInverseMap) throws IOException {
+    private static void computeAddressInverseMap(GOVMinimalPerfectHashFunction<CharSequence> addressMap) throws IOException {
+        logger.info("Computing inverse address map, this might take a while...");
+        Iterator<CharSequence> addresses = Iterators.transform(Utils.readTSVs(addressesFile), line -> Utils.column(line, 0));
+        ProgressLogger progress = new ProgressLogger(LoggerFactory.getLogger(NodeUtility.class));
+        progress.expectedUpdates = addressMap.size64();
+        buildInverseMap(addressMap, addresses, addressesInverseMap, progress);
+        logger.info("Done!");
+    }
+
+    private static void buildInverseMap(GOVMinimalPerfectHashFunction<CharSequence> map, Iterator<CharSequence> iterator, Path addressesInverseMap, ProgressLogger progress) throws IOException {
         Long2ObjectOpenHashMap<String> inverse = new Long2ObjectOpenHashMap<>();
-        iterator.forEachRemaining(line -> inverse.put(map.getLong(line), line.toString()));
+        iterator.forEachRemaining(line -> {
+            inverse.put(map.getLong(line), line.toString());
+            progress.lightUpdate();
+        });
         inverse.trim();
         BinIO.storeObject(inverse, addressesInverseMap.toFile());
     }
