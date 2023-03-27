fe438ee920f1831cb766ca75daad8c3b0efc695a
Luigi Foscari <luigi.foscari@studenti.unimi.it>
2023-03-27 11:13:40 +0200

    added debug, fixed input reading

--- src/main/java/it/unimi/dsi/law/APISanityCheck.java
+++ src/main/java/it/unimi/dsi/law/APISanityCheck.java
@@ -1,87 +1,106 @@
 package it.unimi.dsi.law;
 
 import it.unimi.dsi.fastutil.io.BinIO;
-import it.unimi.dsi.fastutil.io.FastBufferedInputStream;
 import it.unimi.dsi.fastutil.objects.ObjectList;
 import it.unimi.dsi.io.FileLinesByteArrayIterable;
-import it.unimi.dsi.io.FileLinesByteArrayIterable.FileLinesIterator;
 import it.unimi.dsi.logging.ProgressLogger;
 import it.unimi.dsi.sux4j.mph.GOV3Function;
-import it.unimi.dsi.webgraph.ImmutableGraph;
+import it.unimi.dsi.webgraph.EFGraph;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.BitSet;
-import java.util.List;
 import java.util.stream.IntStream;
 
 import static it.unimi.dsi.law.Parameters.*;
 
 public class APISanityCheck {
 	/** Check the correctness of the graph by checking with the output
-	 * of the Blockchair API <a href="https://blockchair.com/api/docs#link_200">Blockchair API</a>.
-	 */
+	 * of the Blockchair API <a href="https://blockchair.com/api/docs#link_200">Blockchair API</a>. */
 
-	private static final Path transactionBaselineFiles = resources.resolve("baseline-transactions/stripped-transactions");
+	private static final boolean DEBUG = false;
+	private static final Path BASELINEFILES = resources.resolve("baseline-transactions/stripped-transactions");
+	private static final ProgressLogger pl = new ProgressLogger(LoggerFactory.getLogger(APISanityCheck.class), "transactions");
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
-		final ProgressLogger pl = new ProgressLogger(LoggerFactory.getLogger(APISanityCheck.class), "transactions");
+		
 		pl.logger.info("Loading graph");
-		final ImmutableGraph graph = ImmutableGraph.loadOffline(basename.toString());
+		final EFGraph graph = EFGraph.loadOffline(basename.toString(), pl);
+		if (!graph.randomAccess()) throw new IllegalArgumentException("Provided graph does not permit random access");
+
 		pl.logger.info("Loading address map");
 		final GOV3Function<byte[]> addressMap = (GOV3Function<byte[]>) BinIO.loadObject(addressesMapFile.toFile());
-		pl.logger.info("Loading transaction map");
-		final GOV3Function<byte[]> transactionMap = (GOV3Function<byte[]>) BinIO.loadObject(transactionsMapFile.toFile());
+		// pl.logger.info("Loading transaction map");
+		// final GOV3Function<byte[]> transactionMap = (GOV3Function<byte[]>) BinIO.loadObject(transactionsMapFile.toFile());
 
-		final File[] transactions = transactionBaselineFiles.toFile().listFiles((d, s) -> s.endsWith(".txt"));
-		if (transactions == null) throw new NoSuchFileException("Not transactions in " + transactionBaselineFiles);
+		final File[] transactions = BASELINEFILES.toFile().listFiles((d, s) -> s.endsWith(".txt"));
+		if (transactions == null) throw new NoSuchFileException("Not transactions in " + BASELINEFILES);
 
 		pl.start("Checking transactions");
 
 		for (File transaction: transactions) {
-			FileLinesByteArrayIterable it = new FileLinesByteArrayIterable(transaction.toString());
+			final FileLinesByteArrayIterable it = new FileLinesByteArrayIterable(transaction.toString());
 			final ObjectList<byte[]> lines = it.allLines();
-			byte[] inputs = lines.get(0), outputs = lines.get(1);
+			final byte[] inputs = lines.get(0), outputs = lines.get(1);
 
 			final String transactionHash = transaction.getName().substring(0, transaction.getName().length() - 4);
 			// final long transactionId = transactionMap.getLong(transactionHash.getBytes());
 
+			final BitSet inputDelim = ArrayUtils.indexesOf(inputs, (byte) 32);
+			inputDelim.set(inputs.length);
+			final BitSet outputDelim = ArrayUtils.indexesOf(outputs, (byte) 32);
+			outputDelim.set(outputs.length);
+
+			if (DEBUG) {
+				System.out.println("\nTransaction file: " + transaction);
+				System.out.println("Transaction: " + transactionHash); // + " (id: " + transactionId + ")");
+				System.out.println("Inputs: " + new String(inputs) + " [" + inputDelim + "]");
+				System.out.println("Outputs: " + new String(outputs) + " [" + outputDelim + "]");
+			}
+
 			int inputOffset = 0;
-			BitSet inputDelim = ArrayUtils.indexesOf(inputs, (byte) 32);
-			BitSet outputDelim = ArrayUtils.indexesOf(outputs, (byte) 32);
-
-			System.out.println(new String(inputs));
-			System.out.println(inputDelim);
-
-			System.out.println(new String(outputs));
-			System.out.println(outputDelim);
-
-			for (int inputSpace: inputDelim.stream().toArray()) {
-				byte[] input = Arrays.copyOfRange(inputs, inputOffset, inputSpace);
-				inputOffset += inputSpace + 1;
+			for (final int inputSpace: inputDelim.stream().toArray()) {
+				final byte[] input = Arrays.copyOfRange(inputs, inputOffset, inputSpace);
 				final long inputId = addressMap.getLong(input);
+				inputOffset = inputSpace + 1;
+
+				System.out.println(new String(input) + " " + inputId);
+
+				if (inputId == addressMap.defaultReturnValue()) {
+					pl.logger.error("Unknown input address " + new String(input) + " in transaction " + transactionHash);
+					continue;
+				}
+
+				if (DEBUG) System.out.print(new String(input) + " -> ");
 
 				int outputOffset = 0;
-				for (int outputSpace: outputDelim.stream().toArray()) {
-					byte[] output = Arrays.copyOfRange(outputs, outputOffset, outputSpace);
-					outputOffset += outputSpace + 1;
+				for (final int outputSpace: outputDelim.stream().toArray()) {
+					final byte[] output = Arrays.copyOfRange(outputs, outputOffset, outputSpace);
 					final long outputId = addressMap.getLong(output);
+					outputOffset = outputSpace + 1;
 
-					final int[] successors = graph.successorArray((int) inputId);
+					if (outputId == addressMap.defaultReturnValue()) {
+						pl.logger.error("Unknown output address " + new String(output) + " in transaction " + transactionHash);
+						break;
+					}
+
+					if (DEBUG) System.out.print(new String(output) + ", ");
+
+					final int[] successors = graph.successorArray((int) inputId); // remove elements after outdegree?
 					if (IntStream.of(successors).noneMatch(s -> s == outputId)) {
 						pl.logger.error("Inconsistency for transaction " + transactionHash + " on input " + input + " and output " + output);
 					}
 
 					pl.lightUpdate();
 				}
+
+				if (DEBUG) System.out.println();
 			}
 		}
 
