87a480ee46fd720e67ef999884c948de8bd381fc
Luigi Foscari <luigi.foscari@icloud.com>
2023-04-06 12:49:58 +0200

    cli parameters refactor

--- src/main/java/it/unimi/dsi/law/Compress.java
+++ src/main/java/it/unimi/dsi/law/Compress.java
@@ -26,11 +26,11 @@ public class Compress {
                 "Compress a given graph using LLP",
                 new Parameter[]{
                         new FlaggedOption("tempDir", JSAP.STRING_PARSER, resources.toString(), JSAP.NOT_REQUIRED, 't', "temp-dir", "A directory for all temporary batch files."),
-                        new FlaggedOption("batchSize", JSAP.INTEGER_PARSER, "1000000", JSAP.NOT_REQUIRED, 'b', "batch-size", "A directory for all temporary batch files."),
-                        new FlaggedOption("clustersDir", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'c', "clusters-dir", "A directory for clusters."),
+                        new FlaggedOption("batchSize", JSAP.INTEGER_PARSER, "10000000", JSAP.NOT_REQUIRED, 'b', "batch-size", "A directory for all temporary batch files."),
+                        new FlaggedOption("clustersDir", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED, 'c', "clusters-dir", "A directory for clusters."),
                         new FlaggedOption("oldBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'i', "basename", "The basename of the input graph."),
                         new FlaggedOption("newBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'o', "new-basename", "The basename of the output graph."),
-                        new Switch("simplify", 's', "simplify", "Perform the simplification (necessary)."),
+                        new Switch("simplify", 's', "simplify", "Perform the simplification and save the resulting graph, this step is necessary for compression."),
                 }
         );
 
@@ -39,24 +39,18 @@ public class Compress {
 
         File tempDir = new File(jsapResult.getString("tempDir"));
         if (!tempDir.exists()) {
-            logger.info(tempDir + " does not exist, creating it");
+            logger.warn(tempDir + " does not exist, creating it");
             tempDir.mkdir();
         }
 
         int batchSize = jsapResult.getInt("batchSize");
-
-        File clustersDir = new File(jsapResult.getString("clustersDir"));
-        if (!clustersDir.exists()) {
-            logger.info(clustersDir + " does not exist, creating it");
-            clustersDir.mkdir();
-        }
 
         String oldBasename = jsapResult.getString("oldBasename");
         String newBasename = jsapResult.getString("newBasename");
 
         File newBasenameDir = new File(newBasename).toPath().getParent().toFile();
         if (!newBasenameDir.exists()) {
-            logger.info(newBasenameDir + " does not exist, creating it");
+            logger.warn(newBasenameDir + " does not exist, creating it");
             newBasenameDir.mkdir();
         }
 
@@ -70,6 +64,14 @@ public class Compress {
             logger.info("Removing loops");
             graph =  Transform.filterArcs(graph, NO_LOOPS, pl);
         } else {
+            if (!jsapResult.contains("clustersDir")) throw new JSAPException("Provide a clusters directory when computing permutations");
+
+            File clustersDir = new File(jsapResult.getString("clustersDir"));
+            if (!clustersDir.exists()) {
+                logger.warn(clustersDir + " does not exist, creating it");
+                clustersDir.mkdir();
+            }
+
             logger.info("Computing permutation");
             LayeredLabelPropagation llp = new LayeredLabelPropagation(graph, SEED);
             int[] permutation = llp.computePermutation(clusterFile.toString());
