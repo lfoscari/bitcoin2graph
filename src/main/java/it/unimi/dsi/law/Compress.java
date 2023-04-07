8473891777363fc0d526e17e9826d3eff541e594
Luigi Foscari <luigi.foscari@icloud.com>
2023-04-07 11:30:40 +0200

    went full online

--- src/main/java/it/unimi/dsi/law/Compress.java
+++ src/main/java/it/unimi/dsi/law/Compress.java
@@ -13,7 +13,6 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 
-import static it.unimi.dsi.law.Parameters.*;
 import static it.unimi.dsi.webgraph.Transform.NO_LOOPS;
 
 public class Compress {
@@ -25,23 +24,13 @@ public class Compress {
         final SimpleJSAP jsap = new SimpleJSAP(ScatteredLabelledArcsASCIIGraph.class.getName(),
                 "Compress a given graph using LLP",
                 new Parameter[]{
-                        new FlaggedOption("tempDir", JSAP.STRING_PARSER, resources.toString(), JSAP.NOT_REQUIRED, 't', "temp-dir", "A directory for all temporary batch files."),
-                        new FlaggedOption("batchSize", JSAP.INTEGER_PARSER, "10000000", JSAP.NOT_REQUIRED, 'b', "batch-size", "A directory for all temporary batch files."),
-                        new FlaggedOption("oldBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'i', "basename", "The basename of the input graph."),
-                        new FlaggedOption("newBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, 'o', "new-basename", "The basename of the output graph."),
+                        new UnflaggedOption("oldBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, false, "The basename of the input graph."),
+                        new UnflaggedOption("newBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, false, "The basename of the output graph."),
                 }
         );
 
         final JSAPResult jsapResult = jsap.parse(args);
         if (jsap.messagePrinted()) System.exit(1);
-
-        File tempDir = new File(jsapResult.getString("tempDir"));
-        if (!tempDir.exists()) {
-            logger.warn(tempDir + " does not exist, creating it");
-            tempDir.mkdir();
-        }
-
-        int batchSize = jsapResult.getInt("batchSize");
 
         String oldBasename = jsapResult.getString("oldBasename");
         String newBasename = jsapResult.getString("newBasename");
@@ -52,27 +41,28 @@ public class Compress {
             newBasenameDir.mkdir();
         }
 
+        File clustersFile = newBasenameDir.toPath().resolve("clusters").toFile();
+
         logger.warn("Loading graph");
-        ImmutableGraph graph = ImmutableGraph.loadMapped(oldBasename, pl);
+        ImmutableGraph graph = ImmutableGraph.load(oldBasename, pl);
 
         logger.warn("Symmetrizing");
-        graph = Transform.symmetrizeOffline(graph, batchSize, tempDir, pl);
+        graph = Transform.symmetrize(graph, pl);
 
         logger.warn("Removing loops");
         graph =  Transform.filterArcs(graph, NO_LOOPS, pl);
 
         logger.warn("Temporarily storing graph");
+        // This is needed to obtain a fully computed union of the graph and its transpose
         BVGraph.store(graph, newBasename, pl);
-        graph = BVGraph.load(newBasename);
-
-        File clustersFile = newBasenameDir.toPath().resolve("clusters").toFile();
+        graph = ImmutableGraph.load(newBasename);
 
         logger.warn("Computing permutation");
         LayeredLabelPropagation llp = new LayeredLabelPropagation(graph, SEED);
         int[] permutation = llp.computePermutation(clustersFile.toString());
 
         logger.warn("Applying permutation");
-        graph = Transform.mapOffline(graph, permutation, batchSize, tempDir, pl);
+        graph = Transform.map(graph, permutation, pl);
 
         logger.warn("Storing graph");
         BVGraph.store(graph, newBasename, pl);
