2054e2be9e887843810aca5b3f686a8f215c432f
Luigi Foscari <luigi.foscari@icloud.com>
2023-04-14 15:20:07 +0200

    llp

---
+++ src/main/java/it/unimi/dsi/law/LLP.java
@@ -0,0 +1,12 @@
+package it.unimi.dsi.law;
+
+import com.martiansoftware.jsap.JSAPException;
+import it.unimi.dsi.law.graph.LayeredLabelPropagation;
+
+import java.io.IOException;
+
+public class LLP {
+	public static void main(String[] args) throws JSAPException, IOException {
+		LayeredLabelPropagation.main(args);
+	}
+}
