fa36741885e00075d6ab084f2d2f0c436cc1c122
Luigi Foscari <luigi.foscari@icloud.com>
2023-04-21 15:42:46 +0200

    added utility to prepare neighbourhood function data for plotting

---
+++ src/main/java/it/unimi/dsi/law/NeighbourhoodPlot.java
@@ -0,0 +1,51 @@
+package it.unimi.dsi.law;
+
+import com.martiansoftware.jsap.*;
+import it.unimi.dsi.fastutil.io.FastBufferedOutputStream;
+import it.unimi.dsi.io.FileLinesMutableStringIterable;
+import it.unimi.dsi.logging.ProgressLogger;
+import it.unimi.dsi.webgraph.ImmutableGraph;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NeighbourhoodPlot {
+    private static final Logger logger = LoggerFactory.getLogger(Simplify.class);
+    private static final ProgressLogger pl = new ProgressLogger(logger);
+
+    public static void main(String[] args) throws JSAPException, IOException {
+        final SimpleJSAP jsap = new SimpleJSAP(NeighbourhoodPlot.class.getName(),
+                "Read a neighbourhood function and save to a file the values normalized by the amount of nodes in the associated graph.",
+                new Parameter[] {new UnflaggedOption("functionFile", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, false, "The function file.")}
+        );
+
+        final JSAPResult jsapResult = jsap.parse(args);
+        if (jsap.messagePrinted()) System.exit(1);
+
+        String ff = jsapResult.getString("functionFile");
+        if (!new File(ff).exists()) throw new JSAPException("File " + ff + " does not exist!");
+
+        FileLinesMutableStringIterable msit = new FileLinesMutableStringIterable(ff);
+        try (PrintStream normalized = new PrintStream(new FastBufferedOutputStream(Files.newOutputStream(Paths.get(ff + ".normalized"))));
+             PrintStream singleValues = new PrintStream(new FastBufferedOutputStream(Files.newOutputStream(Paths.get(ff + ".single"))));
+             FileLinesMutableStringIterable.FileLinesIterator iterator = msit.iterator()) {
+
+            double numNodes = Double.parseDouble(iterator.next().toString());
+            double squaredNumNodes = numNodes * numNodes;
+
+            double previousPairAmount = 0;
+            for (; iterator.hasNext(); iterator.next()) {
+                double pairAmount = Double.parseDouble(iterator.next().toString());
+                normalized.println(pairAmount / squaredNumNodes);
+                singleValues.println((pairAmount - previousPairAmount) / numNodes);
+
+                previousPairAmount = pairAmount;
+            }
+        }
+    }
+}
