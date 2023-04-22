e149d69de438941da598cf1fd5eb9ece84cda150
Luigi Foscari <luigi.foscari@icloud.com>
2023-04-22 12:32:38 +0200

    rollback

--- src/main/java/it/unimi/dsi/law/NeighbourhoodPlot.java
+++ src/main/java/it/unimi/dsi/law/NeighbourhoodPlot.java
@@ -38,8 +38,7 @@ public class NeighbourhoodPlot {
 
             double numNodes = Double.parseDouble(iterator.next().toString());
             double squaredNumNodes = numNodes * numNodes;
-
-            normalized.println(1 / squaredNumNodes);
+            normalized.println(numNodes / squaredNumNodes);
 
             while (iterator.hasNext()) {
                 double pairAmount = Double.parseDouble(iterator.next().toString());
