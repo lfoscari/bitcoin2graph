02134b859d7e9f3096ba968cb7ca46fdebb71e33
Luigi Foscari <luigi.foscari@icloud.com>
2023-05-12 11:52:53 +0200

    now casting single labels, not the array

--- src/main/java/it/unimi/dsi/law/GraphStats.java
+++ src/main/java/it/unimi/dsi/law/GraphStats.java
@@ -1,12 +1,10 @@
 package it.unimi.dsi.law;
 
 import com.martiansoftware.jsap.*;
+import it.unimi.dsi.fastutil.longs.LongList;
 import it.unimi.dsi.fastutil.longs.LongSet;
 import it.unimi.dsi.logging.ProgressLogger;
-import it.unimi.dsi.webgraph.labelling.ArcLabelledImmutableGraph;
-import it.unimi.dsi.webgraph.labelling.ArcLabelledNodeIterator;
-import it.unimi.dsi.webgraph.labelling.BitStreamArcLabelledImmutableGraph;
-import it.unimi.dsi.webgraph.labelling.LongArrayListLabel;
+import it.unimi.dsi.webgraph.labelling.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,8 +40,9 @@ public class GraphStats {
 			it.nextInt();
 			outdegreeMean = (outdegreeMean * nodeCount) + it.outdegree() / (nodeCount + 1);
 
-			for (LongArrayListLabel la: (LongArrayListLabel[]) it.labelArray()) {
-				int unique = LongSet.of(la.values.toLongArray()).size();
+			for (Label la: it.labelArray()) {
+				long[] values = ((LongArrayListLabel) la).values.toLongArray();
+				int unique = LongSet.of(values).size();
 				labelSizeMean = (labelSizeMean * labelCount) + unique / (labelCount + 1);
 			}
 
