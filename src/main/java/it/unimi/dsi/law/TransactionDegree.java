d7f6ed951644efb443dbe17fa6c064e9005c9339
Luigi Foscari <luigi.foscari@icloud.com>
2023-07-07 17:39:29 +0200

    transaction degree

---
+++ src/main/java/it/unimi/dsi/law/TransactionDegree.java
@@ -0,0 +1,60 @@
+package it.unimi.dsi.law;
+
+import com.martiansoftware.jsap.*;
+import it.unimi.dsi.fastutil.io.BinIO;
+import it.unimi.dsi.logging.ProgressLogger;
+import it.unimi.dsi.webgraph.ImmutableGraph;
+import it.unimi.dsi.webgraph.labelling.ArcLabelledImmutableGraph;
+import it.unimi.dsi.webgraph.labelling.ArcLabelledNodeIterator;
+import it.unimi.dsi.webgraph.labelling.Label;
+import it.unimi.dsi.webgraph.labelling.MergeableFixedWidthLongListLabel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class TransactionDegree {
+	private static final Logger logger = LoggerFactory.getLogger(TransactionDegree.class);
+	private static final ProgressLogger pl = new ProgressLogger(logger);
+
+	public static void main(String[] args) throws IOException, JSAPException {
+		final SimpleJSAP jsap = new SimpleJSAP(TransactionDegree.class.getName(), "Compute for each address the number of transactions in which it was involved.",
+				new Parameter[]{
+						new UnflaggedOption("basename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, false, "The basename of the labelled transaction graph."),
+						new UnflaggedOption("basename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, false, "The basename of the labelled transaction graph."),
+						new UnflaggedOption("resultBasename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.NOT_REQUIRED, false, "The basename of the results files."),
+				}
+		);
+
+		final JSAPResult jsapResult = jsap.parse(args);
+		if (jsap.messagePrinted()) System.exit(1);
+
+		final ArcLabelledImmutableGraph g = ArcLabelledImmutableGraph.load(jsapResult.getString("basename"), pl);
+		final int[] transactionInput = new int[g.numNodes()], transactionOutput = new int[g.numNodes()];
+
+		pl.start("Computing transaction cardinality");
+		pl.expectedUpdates = g.numNodes();
+		pl.itemsName = "nodes";
+
+		for (final ArcLabelledNodeIterator it = g.nodeIterator(); it.hasNext(); ) {
+			final int node = it.nextInt();
+			final int[] neighbours = it.successorArray();
+			final Label[] labels = it.labelArray();
+
+			int outputAmount = 0;
+			for (int i = 0; i < it.outdegree(); i++) {
+				final int length = ((MergeableFixedWidthLongListLabel) labels[i]).value.length;
+				outputAmount += length;
+				transactionInput[neighbours[i]] += length;
+			}
+
+			transactionOutput[node] = outputAmount;
+			pl.lightUpdate();
+		}
+
+		pl.done();
+
+		BinIO.storeInts(transactionInput, jsapResult.getString("resultBasename") + ".input");
+		BinIO.storeInts(transactionOutput, jsapResult.getString("resultBasename") + ".output");
+	}
+}
