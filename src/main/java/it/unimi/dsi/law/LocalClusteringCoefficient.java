b3a95302e9186b6ec30a06302ce054f612e361af
Luigi Foscari <luigi.foscari@icloud.com>
2023-08-29 11:02:52 +0200

    refactoring

--- src/main/java/it/unimi/dsi/law/LocalClusteringCoefficient.java
+++ src/main/java/it/unimi/dsi/law/LocalClusteringCoefficient.java
@@ -15,11 +15,12 @@ import java.io.IOException;
 public class LocalClusteringCoefficient {
 	private static final Logger logger = LoggerFactory.getLogger(LocalClusteringCoefficient.class);
 	private static final ProgressLogger pl = new ProgressLogger(logger);
+	private static final String CLUSTERING_EXTENSION = "-clustering.doubles";
 
 	public static void main(String[] args) throws IOException, JSAPException {
 		final SimpleJSAP jsap = new SimpleJSAP(LocalClusteringCoefficient.class.getName(), "Compute the local clustering coefficient for all the nodes in the given graphs according to the number of triangles," +
 				"provided as a file where each row contains the id of a node and its number of triangles, separated by a single space. The results are stored in a file with the same basename with the" +
-				"extension '-clustering.doubles' as an array of doubles.",
+				"extension '" + CLUSTERING_EXTENSION + "' as an array of doubles.",
 				new Parameter[]{
 						new UnflaggedOption("basename", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, false, "The basename of the labelled transaction graph."),
 						new UnflaggedOption("triangles", JSAP.STRING_PARSER, JSAP.NO_DEFAULT, JSAP.REQUIRED, false, "The file containing the number of triangles for each node."),
@@ -63,8 +64,8 @@ public class LocalClusteringCoefficient {
 		}
 		pl.done();
 
-		pl.logger.info("Storing results in " + basename + "-clustering.doubles...");
-		BinIO.storeDoubles(localClusteringCoefficient, basename + "-clustering.doubles");
+		pl.logger.info("Storing results in " + basename + CLUSTERING_EXTENSION + "...");
+		BinIO.storeDoubles(localClusteringCoefficient, basename + CLUSTERING_EXTENSION);
 
 		double mean = 0;
 		for (int node = 0; node < localClusteringCoefficient.length; node++)
